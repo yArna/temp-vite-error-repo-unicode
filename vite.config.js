ba422ff60c6f4dde6a3b6b7c7feb40cf5dc718cd
yArna <yArna@me.moonvy.com>
2021-12-20 22:02:21 +0800

    decode url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        en: resolve(__dirname, "/En/index.html"),
+        zh: resolve(__dirname, "/中文/index.html"),
+      },
+    },
+  },
+});
